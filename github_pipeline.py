0a6114600ed19ebb12938dae3bd7ed0d73b8cf49
xavier.thoraval <xavier.thoraval@lobellia.fr>
2025-05-05 22:12:28 +0200

    gothubactions

--- github_pipeline.py
+++ github_pipeline.py
@@ -1,14 +1,13 @@
 import dlt
 import os
 from dlt.sources.rest_api import rest_api_source
-from itertools import islice
 from dlt.sources.helpers.rest_client import RESTClient
 from dlt.sources.helpers.rest_client.paginators import PageNumberPaginator
 
 
 
 @dlt.source
-def jaffleshop_source(page_size,chunk_size):
+def jaffleshop_source():
     client = RESTClient(
         base_url= "https://jaffle-shop.scalevector.ai/api/v1",
         paginator= PageNumberPaginator (
@@ -20,26 +19,28 @@ def jaffleshop_source(page_size,chunk_size):
 
     @dlt.resource(table_name="orders", write_disposition="replace", parallelized=True)
     def jaffleshop_orders():
-      pages= client.paginate(
+        pages= client.paginate(
           "/orders",
           params={
-              "page_size":page_size,
+              "page_size":500,
               "start_date":"2017-08-01"
           })
-      while page_slice := list(islice(pages, chunk_size)):
-        yield page_slice
+        for resource in pages:
+            yield resource
 
     @dlt.resource(table_name="customers", write_disposition="replace",parallelized=True)
     def jaffleshop_customers():
       pages= client.paginate("/customers")
-      while page_slice := list(islice(pages, chunk_size)):
-        yield page_slice
+      
+      for resource in pages:
+            yield resource
 
     @dlt.resource(table_name="products", write_disposition="replace", parallelized=True)
     def jaffleshop_products():
-      pages= client.paginate("/products")
-      while page_slice := list(islice(pages, chunk_size)):
-        yield page_slice
+        pages= client.paginate("/products")
+
+        for resource in pages:
+            yield resource
 
     return jaffleshop_orders,jaffleshop_customers,jaffleshop_products
 
@@ -50,11 +51,7 @@ pipeline = dlt.pipeline(
 )
 
 
-extract_info = pipeline.extract(jaffleshop_source(100,100))
-print(extract_info)
 
-normalize_info = pipeline.normalize()
-print(normalize_info)
-
-load_info= pipeline.load()
-print(load_info)
+if __name__ == "__main__":
+    load_info= pipeline.run(jaffleshop_source())
+    print(load_info)
